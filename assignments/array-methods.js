8756d6b8a2fc4c130f85893d2a7a22b65f6b9d39
Jamison Blackwell <blackwellj1040@gmail.com>
2019-04-19 22:20:44 -0400

    finished array methods file

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -66,25 +66,46 @@ runners.forEach(function (element) {
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-let caps = runners.map(element => element.first_name.toUpperCase());
-allCaps.push(caps);
+allCaps = runners.map(element => element.first_name.toUpperCase());
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(word => word.shirt_size === "L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
-// The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
+// The donations need to be tallied up and reporte for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
-
+ticketPriceTotal = runners.reduce(function(accumulator, value) {
+    return accumulator + value.donation;
+}, 0);
+ console.log(ticketPriceTotal);
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+let companyYodo = [];
+companyYodo = runners.filter(word => word.company_name === "Yodo");
+console.log(companyYodo);
 // Problem 2
-
+let emails = [];
+runners.forEach(function (element) {
+    emails.push(element.email);
+});
+console.log(emails);
 // Problem 3
+let skinix = [];
+let totalDonationSkinix = [];
+skinix = runners.filter(word => word.company_name === "Skinix");
+let skinixDonations = [];
+skinix.forEach(function (element) {
+    skinixDonations.push(element.donation);
+})
+totalDonationSkinix = skinix.reduce(function (accumulator, value) {
+    return accumulator + value.donation;
+}, 0);
+
+console.log(totalDonationSkinix);
